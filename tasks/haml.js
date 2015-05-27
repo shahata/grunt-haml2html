1c3d990d1aee8f31f97d93d563237ebb688678ef
Shahar Talmi <shahar.talmi@gmail.com>
2015-05-27 12:48:28 +0300

    run in parallel

--- tasks/haml.js
+++ tasks/haml.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var dargs = require('dargs');
+var async = require('async');
+var path = require('path');
 
 module.exports = function(grunt) {
-  var path = require('path');
-
   grunt.registerMultiTask('haml', 'Compile Haml to HTML', function() {
     var options = this.options();
     var cb = this.async();
@@ -17,53 +17,43 @@ module.exports = function(grunt) {
     var encoding = options.encoding;
     delete options.encoding;
 
-    grunt.util.async.forEachSeries(this.files, function(f, next) {
-      var args = ['haml', f.dest, '--stdin'].concat(dargs(options));
+    async.each(this.files, function(f, next) {
+      var filepath = f.src[0];
 
-      if (bundleExec) {
-        args.unshift('bundle', 'exec');
-      }
+      // Warn on and remove invalid source files (if nonull was set).
+      if (!grunt.file.exists(filepath)) {
+        grunt.log.warn('Source file "' + filepath + '" not found.');
+        next();
+      } else {
+        var args = ['haml', f.src[0], f.dest].concat(dargs(options));
 
-      if (encoding) {
-        args.push('-E', encoding);
-      }
-
-      var max = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+        if (bundleExec) {
+          args.unshift('bundle', 'exec');
         }
-      }).map(function(filepath) {
-        // Add dirs of specified files to the haml path
+
+        if (encoding) {
+          args.push('-E', encoding);
+        }
+
         args.push('--load-path', path.dirname(filepath));
 
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(grunt.util.linefeed));
+        // Make sure grunt creates the destination folders
+        grunt.file.write(f.dest, '');
 
-      // Make sure grunt creates the destination folders
-      grunt.file.write(f.dest, '');
-
-      var haml = grunt.util.spawn({
-        cmd: args.shift(),
-        args: args
-      }, function(error, result, code) {
-        if (code === 127) {
-          return grunt.warn(
-            'You need to have Ruby and Haml installed and in your PATH for\n' +
-            'this task to work. More info:\n' +
-            'https://github.com/jhchen/grunt-haml2html'
-          );
-        }
-        next(error);
-      });
-
-      haml.stdin.write(new Buffer(max));
-      haml.stdin.end();
-      haml.stdout.pipe(process.stdout);
-      haml.stderr.pipe(process.stderr);
+        grunt.util.spawn({
+          cmd: args.shift(),
+          args: args
+        }, function(error, result, code) {
+          if (code === 127) {
+            return grunt.warn(
+              'You need to have Ruby and Haml installed and in your PATH for\n' +
+              'this task to work. More info:\n' +
+              'https://github.com/jhchen/grunt-haml2html'
+            );
+          }
+          next(error);
+        });
+      }
     }, cb);
   });
 };
